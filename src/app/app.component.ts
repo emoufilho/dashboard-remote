eb2fa4feedc387258cc21ffa1f607c7529a1d7dd
Eduardo Moura <emoufilho@gmail.com>
2025-11-14 01:19:39 -0300

    inclusao dashbaord

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy, Output, EventEmitter } from '@angular/core';
 import { HelloRemoteComponent } from '../components/hello-remote/hello-remote.component';
 import { ContentRemoteComponent } from '../components/content-remote/content-remote.component';
 
@@ -7,8 +7,21 @@ import { ContentRemoteComponent } from '../components/content-remote/content-rem
   standalone: true,
   imports: [HelloRemoteComponent, ContentRemoteComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
+  private _nome: string = '---';
+  @Input() set nome(value: string) {
+    this._nome = value;
+  }
+  get nome() { return this._nome; }
 
+  contador = 0;
+  @Output() valorChange = new EventEmitter<number>();
+
+  incrementar() {
+    this.contador++;
+    this.valorChange.emit(this.contador);
+  }
 }
